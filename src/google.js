eaca0a4dc15c421a6f165d9f3d17c60bc35a95f8
ThelsK <github@thels.nl>
2019-10-01 10:55:24 +0200

    Converted all API code to promises

--- src/google.js
+++ src/google.js
@@ -2,6 +2,107 @@ const { google } = require("googleapis")
 const fs = require("fs")              // For local file manipulation.
 const readline = require("readline")  // For local file manipulation.
 
+const { CREDENTIALS_PATH, TOKEN_PATH, SCOPES } = require("./settings.js")
+
+const initializeCalendar = async () => {
+
+  console.log("Loading the credentials from a Json file.")
+  let credentials = undefined
+  try {
+    credentials = await loadJsonFile(CREDENTIALS_PATH)
+    console.log("Credentials:", credentials)
+  } catch (error) {
+    console.error("An error occurred while trying to load the " +
+      "credentials from a Json file:", error)
+    return
+  }
+
+  console.log("Creating the Google API client.")
+  let client = undefined
+  try {
+    client = createOAuth2Client(credentials)
+  } catch (error) {
+    console.error("An error occurred while trying to create the " +
+      "Google API client:", error)
+    return
+  }
+
+  console.log("Loading the token from a Json file.")
+  let token = undefined
+  try {
+    token = await loadJsonFile(TOKEN_PATH)
+    console.log("Token:", token)
+  } catch (error) {
+    console.error("An error occurred while trying to load the " +
+      "token from a Json file:", error)
+    console.log("Because the token could not be loaded, a new " +
+      "token will be generated.")
+
+    console.log("Getting the authorization URL.")
+    let authURL = undefined
+    try {
+      authURL = getAuthUrl(client, SCOPES)
+      console.log("Please visit the following site to generate " +
+        "an authorization code:\n", authURL)
+    } catch (error) {
+      console.error("An error occurred while trying to get the " +
+        "authorization URL:", error)
+      return
+    }
+
+    console.log("Asking the user for the authorization code.")
+    let authCode = undefined
+    try {
+      authCode = await askAuthCode("Enter the authorization code: ")
+      console.log("Authorization code:", authCode)
+    } catch (error) {
+      console.error("An error occurred while asking the user " +
+        "for the authorization code:", error)
+      return
+    }
+
+    console.log("Generating the token.")
+    try {
+      token = await generateToken(client, authCode)
+      console.log("Token:", token)
+    } catch (error) {
+      console.error("An error occurred while generating the " +
+        "token:", error)
+      return
+    }
+
+    console.log("Writing the token to a Json file.")
+    try {
+      await writeJsonFile(TOKEN_PATH, token)
+    } catch (error) {
+      console.error("An error occurred while writing the " +
+        "token to a Json file:", error)
+      console.log("Because this is not a critical step, the " +
+        "application will continue to run.")
+    }
+  }
+
+  console.log("Adding the token to the Google API client.")
+  try {
+    addTokenToClient(client, token)
+  } catch (error) {
+    console.error("An error occurred while trying to add the " +
+      "token to the Google API client:", error)
+    return
+  }
+
+  console.log("Google client:", client)
+
+  console.log("Loading the upcoming events.")
+  try {
+    const calendarData = await loadEvents(client)
+    return calendarData
+  } catch (error) {
+    console.error("An error occurred while trying to get the " +
+      "upcoming events:", error)
+  }
+}
+
 // Load data from a Json file
 const loadJsonFile = fileName =>
   new Promise((resolve, reject) => {
@@ -52,7 +153,7 @@ const generateToken = (oAuth2Client, authCode) =>
   new Promise((resolve, reject) => {
     oAuth2Client.getToken(authCode, (error, token) => {
       if (error) reject(error)
-      else resolve(JSON.parse(token))
+      else resolve(token)
     })
   })
 
@@ -63,7 +164,11 @@ const addTokenToClient = (oAuth2Client, token) =>
 // Load the events from the calendar
 const loadEvents = oAuth2Client =>
   new Promise((resolve, reject) => {
-    const calendar = google.calendar({ version: "v3", oAuth2Client })
+    const calendar = google.calendar({
+      auth: oAuth2Client,
+      version: "v3",
+    })
+
     calendar.events.list({
       calendarId: "primary",
       timeMin: (new Date()).toISOString(),
@@ -77,12 +182,5 @@ const loadEvents = oAuth2Client =>
   })
 
 module.exports = {
-  loadJsonFile,
-  writeJsonFile,
-  createOAuth2Client,
-  getAuthUrl,
-  askAuthCode,
-  generateToken,
-  addTokenToClient,
-  loadEvents,
+  initializeCalendar,
 }
--- src/index.js
+++ src/index.js
@@ -1,62 +1,9 @@
-const {
-  loadJsonFile,
-  writeJsonFile,
-  createOAuth2Client,
-  getAuthUrl,
-  askAuthCode,
-  generateToken,
-  addTokenToClient,
-  loadEvents,
-} = require("./google")
-
-const { CREDENTIALS_PATH, TOKEN_PATH, SCOPES } = require("./settings.js")
+// const { initializeCalendar } = require("./example")
+const { initializeCalendar } = require("./google")
 
 const initialize = async () => {
-  let credentials = undefined
-  let token = undefined
-  let client = undefined
-
-  console.log("Loading Credentials.")
-  try {
-    credentials = await loadJsonFile(CREDENTIALS_PATH)
-  } catch (error) {
-    console.error("An error occurred while trying to load the " +
-      "credentials:", error)
-    return
-  }
-  console.log("Credentials:", credentials)
-
-  console.log("Creating Google API client.")
-  const client = createOAuth2Client(credentials)
-
-  console.log("Loading Token.")
-  try {
-    token = await loadJsonFile(TOKEN_PATH)
-  } catch (error) {
-    console.error("An error occurred while trying to load the " +
-      "token:", error)
-
-    console.log("Getting authorization URL.")
-    const authURL = getAuthUrl(client, SCOPES)
-
-    console.log("Please visit the following site to generate " +
-      "an authorization code:/n", authURL)
-
-    const authCode = askAuthCode("Enter the authorization code:")
-    return
-  }
-  console.log("Token:", token)
-
-
-
-  console.log("Loading Token.")
-  try {
-    token = await loadJsonFile(TOKEN_PATH)
-    console.log("Token:", token)
-  } catch (error) {
-    console.error("An error occurred while trying to load the " +
-      "token:", error)
-  }
+  const calendarData = await initializeCalendar()
+  console.log("Events:", calendarData.data.items)
 }
 
 initialize()
