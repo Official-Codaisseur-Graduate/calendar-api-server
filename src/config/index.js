9a986f68df9c9e3c68e10ba09ce64f3197a71f97
ThelsK <github@thels.nl>
2019-10-02 15:23:52 +0200

    Rewriting router

--- src/config/index.js
+++ src/config/index.js
@@ -5,40 +5,18 @@ const Config = require("./model")
 
 const router = new express.Router()
 
-router.get(                                             //GET ALL
-  "/user",
-  (req, res, next) => {
-    User
-      .findAll()
-      .then(user => res.json(user))
-      .catch(err => next(err))
+router.post("/googleapi", (req, res, next) => {
+  const user = {
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 10),
+    name: req.body.name,
+    rank: req.body, rank,
+    validation: req.body.validation
   }
-)
-
-router.post(                                             //CREATE
-  "/user", (req, res, next) => {
-    const user = {
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 10),
-      name: req.body.name,
-      rank: req.body, rank,
-      validation: req.body.validation
-    }
-    User
-      .create(user)
-      .then(newUser => res.json(newUser))
-      .catch(err => next(err))
-  })
-
-router.get(                                               //GET ONE USER
-  "/user/:id",
-  (req, res, next) => {
-    User
-      .findByPk(req.params.id)
-      .then(user => res.json(user))
-      .catch(err => next(err))
-  }
-)
-
+  User
+    .create(user)
+    .then(newUser => res.json(newUser))
+    .catch(err => next(err))
+})
 
 module.exports = router
