cd4b2f22305d29cb85bbc0963bd72c4dd0e124e3
ThelsK <github@thels.nl>
2019-10-02 09:47:32 +0200

    Get events by date

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -1,28 +1,20 @@
 const { Router } = require("express")
 
-const { loadEvents } = require("../google")
+const { loadEvents } = require("./calendarApi")
 
 const router = new Router()
 
 router.get("/events/:year/:month/:day", (req, res, next) => {
-  console.log("Year:", parseInt(req.params.year))
-  console.log("Month:", parseInt(req.params.month) - 1)
-  console.log("Day:", parseInt(req.params.day))
-
   const startDate = new Date(
     parseInt(req.params.year),
     parseInt(req.params.month) - 1,
     parseInt(req.params.day),
     0, 0, 0, 0 // Set time of day to start of day.
   )
-
   const endDate = new Date(startDate)
   endDate.setDate(endDate.getDate() + 1)
 
-  console.log("Start Date:", startDate.toString())
-  console.log("End Date:", endDate.toString())
-
-  loadEvents()
+  loadEvents(startDate, endDate)
     .then(data => {
       const events = data.data.items
       res.send(events.map(event => {
