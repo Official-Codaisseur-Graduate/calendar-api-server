0645315c850eb084478d2d41e78c95a1070c8e4e
gijsmaas82 <gijsmaas82@outlook.com>
2019-10-03 16:40:18 +0200

    mail send to user with confirmationlink

--- src/user/index.js
+++ src/user/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const router = new Router()
 const User = require('./model')
 const bcrypt = require('bcrypt')
+const nodemailer = require('nodemailer')
 
 router.get(                                             //GET ALL
   '/user',
@@ -12,6 +13,34 @@ router.get(                                             //GET ALL
       .catch(err => next(err))
   }
 )
+
+router.post(                                             //CREATE
+  '/user', (req, res, next) => {
+    const email = req.body.email
+    const password = req.body.password
+    const user = {
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 10),
+      name: req.body.name,
+      rank: req.body.rank,
+      validation: req.body.validation
+    }
+    if (!email || !password) {
+      res.status(400).send({ message: 'Please enter a valid email and password' })
+    } else {
+      User
+        .findOne({ where: { email: req.body.email } })
+        .then(entity => {
+          if (!entity) {
+            res.status(400).send({ message: 'User with that email does not exist' })
+          }
+        })
+    }
+    User
+      .create(user)
+      .then(newUser => res.json(newUser))
+      .catch(err => next(err))
+  })
 
 router.get(                                               //GET ONE USER
   '/user/:id',
