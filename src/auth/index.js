82f390aa89132ac6e2852fb9e64c5f376e41123c
krik-chry <krikonis.chrisostomos@gmail.com>
2019-12-03 16:05:05 +0100

    Login router

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,35 +1,37 @@
-const { Router } = require("express")
-const bcrypt = require("bcryptjs")
+const { Router } = require('express');
+const bcrypt = require('bcryptjs');
 
-const User = require("../user/model")
-const validate = require("./validate")
-const { toJWT } = require("./jwt")
-const { getEmailCredentials } = require("../sendEmail/middleware")
-const { checkEmail, checkString } = require("../checkData")
-const randomCode = require("../randomCode")
-const { sendRegisterEmail, alreadyRegisteredEmail, ResetPassword } = require("../sendEmail")
-const { superAdmin } = require("./superAdmin")
+const User = require('../user/model');
+const validate = require('./validate');
+const { toJWT } = require('./jwt');
+const { getEmailCredentials } = require('../sendEmail/middleware');
+const { checkEmail, checkString } = require('../checkData');
+const randomCode = require('../randomCode');
+const {
+  sendRegisterEmail,
+  alreadyRegisteredEmail,
+  ResetPassword
+} = require('../sendEmail');
+const { superAdmin } = require('./superAdmin');
 
-const router = new Router()
+const router = new Router();
 
-
-router.get("/validation", validate, async (req, res) => {
-  console.log("req.user.password / validation", req.user.password)
-  console.log("req.user.newEmail / validation", req.user.newEmail)
+router.get('/validation', validate, async (req, res) => {
+  console.log('req.user.password / validation', req.user.password);
+  console.log('req.user.newEmail / validation', req.user.newEmail);
   try {
-
     // If the user does not have a password set for their account,
     // then the user must still be in the registration process.
 
     if (!req.user.password) {
       return res.send({
-        validationType: "register",
+        validationType: 'register',
         user: {
           id: req.user.id,
           email: req.user.email,
-          rank: req.user.rank,
-        },
-      })
+          rank: req.user.rank
+        }
+      });
     }
 
     // If the user does not have a newEmail set for their account,
@@ -37,14 +39,14 @@ router.get("/validation", validate, async (req, res) => {
 
     if (!req.user.newEmail) {
       return res.send({
-        validationType: "resetPassword",
+        validationType: 'resetPassword',
         user: {
           id: req.user.id,
           email: req.user.email,
           name: req.user.name,
-          rank: req.user.rank,
-        },
-      })
+          rank: req.user.rank
+        }
+      });
     }
 
     // If the user has a newEmail set for their account,
@@ -54,126 +56,120 @@ router.get("/validation", validate, async (req, res) => {
     const checkUser = await User.findOne({
       where: {
         email: req.user.newEmail
-      },
-    })
+      }
+    });
     if (checkUser) {
       user.update({
         newEmail: null,
-        validation: null,
-      })
+        validation: null
+      });
       return res.status(400).send({
-        message: "Email address in use by other account.",
-        user: {},
-      })
+        message: 'Email address in use by other account.',
+        user: {}
+      });
     }
 
     return res.send({
-      validationType: "changeEmail",
+      validationType: 'changeEmail',
       user: {
         id: req.user.id,
         email: req.user.email,
         name: req.user.name,
         rank: req.user.rank,
-        newEmail: req.user.newEmail,
-      },
-    })
-
+        newEmail: req.user.newEmail
+      }
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return res.status(500).send({
-      message: "Internal server error.",
-    })
+      message: 'Internal server error.'
+    });
   }
-})
-
+});
 
 // gets the email credentials finds the user with the email and creates a user in user model.
-router.post("/register", getEmailCredentials, async (req, res) => {
+router.post('/register', getEmailCredentials, async (req, res) => {
   try {
-
     if (!checkEmail(req.body.email)) {
       return res.status(400).send({
-        message: "'email' must be an email address.",
-      })
+        message: "'email' must be an email address."
+      });
     }
 
     let user = await User.findOne({
       where: {
         email: req.body.email
-      },
-    })
+      }
+    });
     if (!user) {
       user = await User.create({
         email: req.body.email,
-        validation: randomCode(),
-      })
+        validation: randomCode()
+      });
     }
 
     // If the user does not have a password set for their account,
     // then the user must still be in the registration process.
 
     if (!user.password) {
-      await sendRegisterEmail(req.transport, user.email, user.validation)
+      await sendRegisterEmail(req.transport, user.email, user.validation);
     } else {
-      await alreadyRegisteredEmail(req.transport, user.email)
+      await alreadyRegisteredEmail(req.transport, user.email);
     }
 
-    // Always return the following "verification email sent" message, 
+    // Always return the following "verification email sent" message,
     // regardless whether the email is already found in the database,
     // So users cannot go fish for which emails do or do not exist.
     // The actual email that was sent above informs the user.
 
     return res.send({
-      message: "Verification email sent. Check your email to continue.",
-    })
-
+      message: 'Verification email sent. Check your email to continue.'
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return res.status(500).send({
-      message: "Internal server error.",
-    })
+      message: 'Internal server error.'
+    });
   }
-})
-
+});
 
 // SignUp validation after you click on the link from the email
-router.post("/registervalidation", validate, async (req, res) => {
+router.post('/registervalidation', validate, async (req, res) => {
   try {
-
     // check if password is 8 characters long
     if (!checkString(req.body.password, 8)) {
       return res.status(400).send({
-        message: "'password' must be a password with at least " +
-          "8 characters.",
-      })
+        message:
+          "'password' must be a password with at least " + '8 characters.'
+      });
     }
 
     if (req.body.password === req.user.email) {
       return res.status(400).send({
-        message: "'password' cannot be identical to email address.",
-      })
+        message: "'password' cannot be identical to email address."
+      });
     }
 
     // check the name if it alteast has 2 characters and less than 40 characters
     if (!checkString(req.body.name, 2, 40)) {
       return res.status(400).send({
-        message: "'name' must be a string with at at least " +
-          "2 and at most 40 characters.",
-      })
+        message:
+          "'name' must be a string with at at least " +
+          '2 and at most 40 characters.'
+      });
     }
 
     // storing the password by hashing it
-    const encryptedPassword = await bcrypt
-      .hashSync(req.body.password, 10)
+    const encryptedPassword = await bcrypt.hashSync(req.body.password, 10);
     req.user.update({
       password: encryptedPassword,
       name: req.body.name,
-      validation: null,
-    })
+      validation: null
+    });
 
     // sends the jwt to the frontend
     return res.send({
-      message: "User account registered.",
+      message: 'User account registered.',
       user: {
         id: req.user.id,
         email: req.user.email,
@@ -181,63 +177,16 @@ router.post("/registervalidation", validate, async (req, res) => {
         rank: req.user.rank,
         jwt: toJWT({
           userId: req.user.id
-        }),
-      },
-    })
-
+        })
+      }
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return res.status(500).send({
-      message: "Internal server error.",
-    })
+      message: 'Internal server error.'
+    });
   }
-})
-
-// router.post("/register", async (req, res) => {
-//   try {
-
-//     if (!checkEmail(req.body.email)) {
-//       return res.status(400).send({
-//         message: "'email' must be an email address.",
-//       })
-//     }
-
-//     let user = await User.findOne({
-//       where: { email: req.body.email },
-//     })
-//     if (!user) {
-//       user = await User.create({
-//         email: req.body.email,
-//         validation: randomCode(),
-//       })
-//     }
-
-//     // If the user does not have a password set for their account,
-//     // then the user must still be in the registration process.
-
-//     if (!user.password) {
-//       await sendRegisterEmail(user.email, user.validation)
-//     } else {
-//       await alreadyRegisteredEmail(user.email)
-//     }
-
-//     // Always return the following "verification email sent" message, 
-//     // regardless whether the email is already found in the database,
-//     // So users cannot go fish for which emails do or do not exist.
-//     // The actual email that was sent above informs the user.
-
-//     return res.send({
-//       message: "Verification email sent. Check your email to continue.",
-//     })
-
-//   } catch (error) {
-//     console.error(error)
-//     return res.status(500).send({
-//       message: "Internal server error.",
-//     })
-//   }
-// })
-
+});
 
 /* 
    The login endpoint does the following:
@@ -250,23 +199,23 @@ router.post("/registervalidation", validate, async (req, res) => {
            -- JWT Token
 */
 
-
-router.post("/login", async (req, res) => {
-  console.log("req.body", req.body)
+router.post('/login', async (req, res) => {
+  console.log('req.body', req.body);
   try {
-
     if (!checkString(req.body.email)) {
       return res.status(400).send({
-        message: "'email' must be the email address of the " +
-          "user that is logging in.",
-      })
+        message:
+          "'email' must be the email address of the " +
+          'user that is logging in.'
+      });
     }
 
     if (!checkString(req.body.password)) {
       return res.status(400).send({
-        message: "'password' must be a valid password for the " +
-          "user that is logging in.",
-      })
+        message:
+          "'password' must be a valid password for the " +
+          'user that is logging in.'
+      });
     }
 
     // Both of the following checks return the same error message,
@@ -275,28 +224,30 @@ router.post("/login", async (req, res) => {
     const user = await User.findOne({
       where: {
         email: req.body.email
-      },
-    })
+      }
+    });
     if (!user) {
       return res.status(400).send({
-        message: "Email address not found or password incorrect.",
-      })
+        message: 'Not existing user with this email. Try again!'
+      });
     }
 
-    const comparePassword = await bcrypt
-      .compareSync(req.body.password, user.password)
+    const comparePassword = await bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
     if (!comparePassword) {
       return res.status(400).send({
-        message: "Email address not found or password incorrect.",
-      })
+        message: 'Incorrect password. Try again!'
+      });
     }
 
     // If this is the Super Admin, force rank to 4.
 
     if (user.email === superAdmin && user.rank < 4) {
       await user.update({
-        rank: 4,
-      })
+        rank: 4
+      });
     }
 
     return res.send({
@@ -307,19 +258,16 @@ router.post("/login", async (req, res) => {
         rank: user.rank,
         jwt: toJWT({
           userId: user.id
-        }),
-      },
-    })
-
+        })
+      }
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return res.status(500).send({
-      message: "Internal server error.",
-    })
+      message: 'Internal server error.'
+    });
   }
-})
-
-
+});
 
 // finds the user with the email
 
@@ -328,38 +276,34 @@ router.post("/login", async (req, res) => {
 // get message "Verification email sent. Check your email to continue."
 
 // if not will get 400 HTTP status code and message "Email address not found"
-router.post("/forgot-password", getEmailCredentials, async (req, res) => {
+router.post('/forgot-password', getEmailCredentials, async (req, res) => {
   try {
     let user = await User.findOne({
       where: {
         email: req.body.email
-      },
-    })
+      }
+    });
     if (!user) {
       return res.status(400).send({
-        message: "Email address not found",
-      })
+        message: 'Email address not found'
+      });
     } else {
       user.update({
         validation: randomCode()
-      })
-      await ResetPassword(req.transport, user.email, user.validation)
+      });
+      await ResetPassword(req.transport, user.email, user.validation);
     }
 
     return res.send({
-      message: "Verification email sent. Check your email to continue.",
-    })
-
+      message: 'Verification email sent. Check your email to continue.'
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return res.status(500).send({
-      message: "Internal server error.",
-    })
+      message: 'Internal server error.'
+    });
   }
-})
-
-
-
+});
 
 // finds the user with the email
 
@@ -377,38 +321,39 @@ router.post('/reset-password', validate, async (req, res) => {
     let user = await User.findOne({
       where: {
         email: req.body.email
-      },
-    })
+      }
+    });
     // console.log("USER", user.email)
     if (user.email) {
       // check if the new password is 8 characters long
       // set validation to null
       if (!checkString(req.body.new_password, 8)) {
         return res.status(400).send({
-          message: "'password' must be a password with at least " +
-            "8 characters.",
-        })
+          message:
+            "'password' must be a password with at least " + '8 characters.'
+        });
       } else {
         // storing the new password by hashing it
-        const encryptedPassword = await bcrypt
-          .hashSync(req.body.new_password, 10)
+        const encryptedPassword = await bcrypt.hashSync(
+          req.body.new_password,
+          10
+        );
         //Reset old password to new password
         user.update({
           password: encryptedPassword,
-          validation: null,
-        })
+          validation: null
+        });
         return res.status(200).send({
-          message: "Password has changed",
-        })
+          message: 'Password has changed'
+        });
       }
     }
-
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return res.status(500).send({
-      message: "Internal server error.",
-    })
+      message: 'Internal server error.'
+    });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
--- src/calendar/index.js
+++ src/calendar/index.js
@@ -1,88 +1,88 @@
-const { Router } = require("express")
-const auth = require('../auth/auth')
-const Config = require("../config/model")
-const { getCalendar, getCalendarId } = require("./middleware")
+const { Router } = require('express');
+const auth = require('../auth/auth');
+const Config = require('../config/model');
+const { getCalendar, getCalendarId } = require('./middleware');
 
-const router = new Router()
+const router = new Router();
 
-
-router.get("/calendars", getCalendar, async (req, res) => {
+router.get('/calendars', getCalendar, async (req, res) => {
   try {
-
     if (req.user.rank < 4) {
       return res.status(403).send({
-        message: "Only admin users can see all calendar IDs.",
-      })
+        message: 'Only admin users can see all calendar IDs.'
+      });
     }
 
     const calendar_id_entry = await Config.findOne({
-      where: { key: "calendar_id" }
-    })
+      where: { key: 'calendar_id' }
+    });
 
     req.calendar.calendarList.list(
       {
         showDeleted: true,
-        showHidden: true,
+        showHidden: true
       },
 
       (error, result) => {
         if (error) {
-          console.error(error)
+          console.error(error);
           return res.status(500).send({
-            message: "Internal server error."
-          })
+            message: 'Internal server error.'
+          });
         }
 
         res.send({
           events: result.data.items.map(calendar => {
-            if (calendar_id_entry &&
-              calendar.id === calendar_id_entry.data) {
+            if (calendar_id_entry && calendar.id === calendar_id_entry.data) {
               return {
                 id: calendar.id,
                 summary: calendar.summary,
-                selected: true,
-              }
+                selected: true
+              };
             }
             return {
               id: calendar.id,
-              summary: calendar.summary,
-            }
+              summary: calendar.summary
+            };
           })
-        })
+        });
       }
-    )
-
+    );
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return res.status(500).send({
-      message: "Internal server error.",
-    })
+      message: 'Internal server error.'
+    });
   }
-})
-
+});
 
 /* this endpoint shows all the events of the current date if the calendar Id is setup
    Initially  while setting up, it will throw an error because the calendar Id is not setup
 */
-router.get("/events/:year/:month/:day", auth,
-  getCalendar, getCalendarId, (req, res) => {
-
+router.get(
+  '/events/:year/:month/:day',
+  auth,
+  getCalendar,
+  getCalendarId,
+  (req, res) => {
     try {
-
       if (!req.user.rank) {
         return res.status(403).send({
-          message: "Only authorized users can load calendar data.",
-        })
+          message: 'Only authorized users can load calendar data.'
+        });
       }
 
       const startDate = new Date(
         parseInt(req.params.year),
         parseInt(req.params.month) - 1,
         parseInt(req.params.day),
-        0, 0, 0, 0 // Set time of day to start of day.
-      )
-      const endDate = new Date(startDate)
-      endDate.setDate(endDate.getDate() + 1)
+        0,
+        0,
+        0,
+        0 // Set time of day to start of day.
+      );
+      const endDate = new Date(startDate);
+      endDate.setDate(endDate.getDate() + 1);
 
       req.calendar.events.list(
         {
@@ -90,35 +90,30 @@ router.get("/events/:year/:month/:day", auth,
           timeMin: startDate.toISOString(),
           timeMax: endDate.toISOString(),
           singleEvents: true,
-          orderBy: "starttime",
+          orderBy: 'starttime'
         },
 
         (error, result) => {
           if (error) {
-            console.error(error)
+            console.error(error);
             return res.status(500).send({
-              message: "Error"
-            })
+              message: 'Error'
+            });
           }
-// res.json the below line
           res.send({
             events: result.data.items.map(event => ({
               ...event
-              // summary: event.summary,
-              // description: event.description,
-              // start: event.start.dateTime,
-              // end: event.end.dateTime
             }))
-          })
+          });
         }
-      )
-
+      );
     } catch (error) {
-      console.error(error)
+      console.error(error);
       return res.status(500).send({
-        message: "error Check.",
-      })
+        message: 'error Check.'
+      });
     }
-  })
+  }
+);
 
-module.exports = router
+module.exports = router;
