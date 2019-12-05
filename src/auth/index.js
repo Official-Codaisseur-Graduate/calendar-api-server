7c3e73a419d50430c1e5e0d38bb0ebefe025554a
Simona Winnekes <simona.winnekes@googlemail.com>
2019-12-05 10:50:17 +0100

    add default value to profile pic

--- src/auth/index.js
+++ src/auth/index.js
@@ -8,184 +8,185 @@ const { getEmailCredentials } = require('../sendEmail/middleware');
 const { checkEmail, checkString } = require('../checkData');
 const randomCode = require('../randomCode');
 const {
-  sendRegisterEmail,
-  alreadyRegisteredEmail,
-  ResetPassword
+    sendRegisterEmail,
+    alreadyRegisteredEmail,
+    ResetPassword,
 } = require('../sendEmail');
 const { superAdmin } = require('./superAdmin');
 
 const router = new Router();
 
 router.get('/validation', validate, async (req, res) => {
-  console.log('req.user.password / validation', req.user.password);
-  console.log('req.user.newEmail / validation', req.user.newEmail);
-  try {
-    // If the user does not have a password set for their account,
-    // then the user must still be in the registration process.
+    console.log('req.user.password / validation', req.user.password);
+    console.log('req.user.newEmail / validation', req.user.newEmail);
+    try {
+        // If the user does not have a password set for their account,
+        // then the user must still be in the registration process.
 
-    if (!req.user.password) {
-      return res.send({
-        validationType: 'register',
-        user: {
-          id: req.user.id,
-          email: req.user.email,
-          rank: req.user.rank
+        if (!req.user.password) {
+            return res.send({
+                validationType: 'register',
+                user: {
+                    id: req.user.id,
+                    email: req.user.email,
+                    rank: req.user.rank,
+                },
+            });
         }
-      });
-    }
 
-    // If the user does not have a newEmail set for their account,
-    // then the user must be resetting their password.
+        // If the user does not have a newEmail set for their account,
+        // then the user must be resetting their password.
 
-    if (!req.user.newEmail) {
-      return res.send({
-        validationType: 'resetPassword',
-        user: {
-          id: req.user.id,
-          email: req.user.email,
-          name: req.user.name,
-          rank: req.user.rank
+        if (!req.user.newEmail) {
+            return res.send({
+                validationType: 'resetPassword',
+                user: {
+                    id: req.user.id,
+                    email: req.user.email,
+                    name: req.user.name,
+                    rank: req.user.rank,
+                },
+            });
         }
-      });
+
+        // If the user has a newEmail set for their account,
+        // then the user must be changing their email address.
+        // Check if someone already uses that new email address.
+
+        const checkUser = await User.findOne({
+            where: {
+                email: req.user.newEmail,
+            },
+        });
+        if (checkUser) {
+            user.update({
+                newEmail: null,
+                validation: null,
+            });
+            return res.status(400).send({
+                message: 'Email address in use by other account.',
+                user: {},
+            });
+        }
+
+        return res.send({
+            validationType: 'changeEmail',
+            user: {
+                id: req.user.id,
+                email: req.user.email,
+                name: req.user.name,
+                rank: req.user.rank,
+                newEmail: req.user.newEmail,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({
+            message: 'Internal server error.',
+        });
     }
-
-    // If the user has a newEmail set for their account,
-    // then the user must be changing their email address.
-    // Check if someone already uses that new email address.
-
-    const checkUser = await User.findOne({
-      where: {
-        email: req.user.newEmail
-      }
-    });
-    if (checkUser) {
-      user.update({
-        newEmail: null,
-        validation: null
-      });
-      return res.status(400).send({
-        message: 'Email address in use by other account.',
-        user: {}
-      });
-    }
-
-    return res.send({
-      validationType: 'changeEmail',
-      user: {
-        id: req.user.id,
-        email: req.user.email,
-        name: req.user.name,
-        rank: req.user.rank,
-        newEmail: req.user.newEmail
-      }
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send({
-      message: 'Internal server error.'
-    });
-  }
 });
 
 // gets the email credentials finds the user with the email and creates a user in user model.
 router.post('/register', getEmailCredentials, async (req, res) => {
-  try {
-    if (!checkEmail(req.body.email)) {
-      return res.status(400).send({
-        message: 'Mail must be in format 123@abc.com.'
-      });
+    try {
+        if (!checkEmail(req.body.email)) {
+            return res.status(400).send({
+                message: 'Mail must be in format 123@abc.com.',
+            });
+        }
+
+        let user = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+        if (!user) {
+            user = await User.create({
+                email: req.body.email,
+                validation: randomCode(),
+            });
+        }
+
+        // If the user does not have a password set for their account,
+        // then the user must still be in the registration process.
+
+        if (!user.password) {
+            await sendRegisterEmail(req.transport, user.email, user.validation);
+        } else {
+            await alreadyRegisteredEmail(req.transport, user.email);
+        }
+
+        // Always return the following "verification email sent" message,
+        // regardless whether the email is already found in the database,
+        // So users cannot go fish for which emails do or do not exist.
+        // The actual email that was sent above informs the user.
+
+        return res.send({
+            message: 'Verification email sent. Check your email to continue.',
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({
+            message: 'Internal server error.',
+        });
     }
-
-    let user = await User.findOne({
-      where: {
-        email: req.body.email
-      }
-    });
-    if (!user) {
-      user = await User.create({
-        email: req.body.email,
-        validation: randomCode()
-      });
-    }
-
-    // If the user does not have a password set for their account,
-    // then the user must still be in the registration process.
-
-    if (!user.password) {
-      await sendRegisterEmail(req.transport, user.email, user.validation);
-    } else {
-      await alreadyRegisteredEmail(req.transport, user.email);
-    }
-
-    // Always return the following "verification email sent" message,
-    // regardless whether the email is already found in the database,
-    // So users cannot go fish for which emails do or do not exist.
-    // The actual email that was sent above informs the user.
-
-    return res.send({
-      message: 'Verification email sent. Check your email to continue.'
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send({
-      message: 'Internal server error.'
-    });
-  }
 });
 
 // SignUp validation after you click on the link from the email
 router.post('/registervalidation', validate, async (req, res) => {
-  try {
-    // check if password is 8 characters long
-    if (!checkString(req.body.password, 8)) {
-      return res.status(400).send({
-        message:
-          "'password' must be a password with at least " + '8 characters.'
-      });
+    try {
+        // check if password is 8 characters long
+        if (!checkString(req.body.password, 8)) {
+            return res.status(400).send({
+                message:
+                    "'password' must be a password with at least " +
+                    '8 characters.',
+            });
+        }
+
+        if (req.body.password === req.user.email) {
+            return res.status(400).send({
+                message: "'password' cannot be identical to email address.",
+            });
+        }
+
+        // check the name if it alteast has 2 characters and less than 40 characters
+        if (!checkString(req.body.name, 2, 40)) {
+            return res.status(400).send({
+                message:
+                    "'name' must be a string with at at least " +
+                    '2 and at most 40 characters.',
+            });
+        }
+
+        // storing the password by hashing it
+        const encryptedPassword = await bcrypt.hashSync(req.body.password, 10);
+        req.user.update({
+            password: encryptedPassword,
+            name: req.body.name,
+            validation: null,
+        });
+
+        // sends the jwt to the frontend
+        return res.send({
+            message: 'User account registered.',
+            user: {
+                id: req.user.id,
+                email: req.user.email,
+                name: req.user.name,
+                rank: req.user.rank,
+                jwt: toJWT({
+                    userId: req.user.id,
+                }),
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({
+            message: 'Internal server error.',
+        });
     }
-
-    if (req.body.password === req.user.email) {
-      return res.status(400).send({
-        message: "'password' cannot be identical to email address."
-      });
-    }
-
-    // check the name if it alteast has 2 characters and less than 40 characters
-    if (!checkString(req.body.name, 2, 40)) {
-      return res.status(400).send({
-        message:
-          "'name' must be a string with at at least " +
-          '2 and at most 40 characters.'
-      });
-    }
-
-    // storing the password by hashing it
-    const encryptedPassword = await bcrypt.hashSync(req.body.password, 10);
-    req.user.update({
-      password: encryptedPassword,
-      name: req.body.name,
-      validation: null
-    });
-
-    // sends the jwt to the frontend
-    return res.send({
-      message: 'User account registered.',
-      user: {
-        id: req.user.id,
-        email: req.user.email,
-        name: req.user.name,
-        rank: req.user.rank,
-        jwt: toJWT({
-          userId: req.user.id
-        })
-      }
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send({
-      message: 'Internal server error.'
-    });
-  }
 });
 
 /* 
@@ -200,73 +201,74 @@ router.post('/registervalidation', validate, async (req, res) => {
 */
 
 router.post('/login', async (req, res) => {
-  console.log('req.body', req.body);
-  try {
-    if (!checkString(req.body.email)) {
-      return res.status(400).send({
-        message:
-          "'email' must be the email address of the " +
-          'user that is logging in.'
-      });
+    console.log('req.body', req.body);
+    try {
+        if (!checkString(req.body.email)) {
+            return res.status(400).send({
+                message:
+                    "'email' must be the email address of the " +
+                    'user that is logging in.',
+            });
+        }
+
+        if (!checkString(req.body.password)) {
+            return res.status(400).send({
+                message:
+                    "'password' must be a valid password for the " +
+                    'user that is logging in.',
+            });
+        }
+
+        // Both of the following checks return the same error message,
+        // So users cannot go fish for which emails do or do not exist.
+
+        const user = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+        if (!user) {
+            return res.status(400).send({
+                message: 'Not existing user with this email. Try again!',
+            });
+        }
+
+        const comparePassword = await bcrypt.compareSync(
+            req.body.password,
+            user.password
+        );
+        if (!comparePassword) {
+            return res.status(400).send({
+                message: 'Incorrect password. Try again!',
+            });
+        }
+
+        // If this is the Super Admin, force rank to 4.
+
+        if (user.email === superAdmin && user.rank < 4) {
+            await user.update({
+                rank: 4,
+            });
+        }
+
+        return res.send({
+            user: {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                profilePic: user.profilePic,
+                rank: user.rank,
+                jwt: toJWT({
+                    userId: user.id,
+                }),
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({
+            message: 'Internal server error.',
+        });
     }
-
-    if (!checkString(req.body.password)) {
-      return res.status(400).send({
-        message:
-          "'password' must be a valid password for the " +
-          'user that is logging in.'
-      });
-    }
-
-    // Both of the following checks return the same error message,
-    // So users cannot go fish for which emails do or do not exist.
-
-    const user = await User.findOne({
-      where: {
-        email: req.body.email
-      }
-    });
-    if (!user) {
-      return res.status(400).send({
-        message: 'Not existing user with this email. Try again!'
-      });
-    }
-
-    const comparePassword = await bcrypt.compareSync(
-      req.body.password,
-      user.password
-    );
-    if (!comparePassword) {
-      return res.status(400).send({
-        message: 'Incorrect password. Try again!'
-      });
-    }
-
-    // If this is the Super Admin, force rank to 4.
-
-    if (user.email === superAdmin && user.rank < 4) {
-      await user.update({
-        rank: 4
-      });
-    }
-
-    return res.send({
-      user: {
-        id: user.id,
-        email: user.email,
-        name: user.name,
-        rank: user.rank,
-        jwt: toJWT({
-          userId: user.id
-        })
-      }
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send({
-      message: 'Internal server error.'
-    });
-  }
 });
 
 // finds the user with the email
@@ -277,36 +279,36 @@ router.post('/login', async (req, res) => {
 
 // if not will get 400 HTTP status code and message "Email address not found"
 router.post('/forgot-password', getEmailCredentials, async (req, res) => {
-  try {
-    let user = await User.findOne({
-      where: {
-        email: req.body.email
-      }
-    });
-    if (!checkEmail(req.body.email)) {
-      return res.send({
-        message: 'This is not a valid email format.'
-      });
-    } else if (!user) {
-      return res.send({
-        message: 'Email address not found'
-      });
-    } else {
-      user.update({
-        validation: randomCode()
-      });
-      await ResetPassword(req.transport, user.email, user.validation);
-    }
+    try {
+        let user = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+        if (!checkEmail(req.body.email)) {
+            return res.send({
+                message: 'This is not a valid email format.',
+            });
+        } else if (!user) {
+            return res.send({
+                message: 'Email address not found',
+            });
+        } else {
+            user.update({
+                validation: randomCode(),
+            });
+            await ResetPassword(req.transport, user.email, user.validation);
+        }
 
-    return res.send({
-      message: 'Verification email sent. Check your email to continue.'
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send({
-      message: 'Internal server error.'
-    });
-  }
+        return res.send({
+            message: 'Verification email sent. Check your email to continue.',
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({
+            message: 'Internal server error.',
+        });
+    }
 });
 
 // finds the user with the email
@@ -320,44 +322,45 @@ router.post('/forgot-password', getEmailCredentials, async (req, res) => {
 
 // the link can change to new password only one time
 router.post('/reset-password', validate, async (req, res) => {
-  // console.log("RESET PASSWORD", req.body)
-  try {
-    let user = await User.findOne({
-      where: {
-        email: req.body.email
-      }
-    });
-    // console.log("USER", user.email)
-    if (user.email) {
-      // check if the new password is 8 characters long
-      // set validation to null
-      if (!checkString(req.body.new_password, 8)) {
-        return res.status(400).send({
-          message:
-            "'password' must be a password with at least " + '8 characters.'
+    // console.log("RESET PASSWORD", req.body)
+    try {
+        let user = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
         });
-      } else {
-        // storing the new password by hashing it
-        const encryptedPassword = await bcrypt.hashSync(
-          req.body.new_password,
-          10
-        );
-        //Reset old password to new password
-        user.update({
-          password: encryptedPassword,
-          validation: null
+        // console.log("USER", user.email)
+        if (user.email) {
+            // check if the new password is 8 characters long
+            // set validation to null
+            if (!checkString(req.body.new_password, 8)) {
+                return res.status(400).send({
+                    message:
+                        "'password' must be a password with at least " +
+                        '8 characters.',
+                });
+            } else {
+                // storing the new password by hashing it
+                const encryptedPassword = await bcrypt.hashSync(
+                    req.body.new_password,
+                    10
+                );
+                //Reset old password to new password
+                user.update({
+                    password: encryptedPassword,
+                    validation: null,
+                });
+                return res.status(200).send({
+                    message: 'Password has changed',
+                });
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({
+            message: 'Internal server error.',
         });
-        return res.status(200).send({
-          message: 'Password has changed'
-        });
-      }
     }
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send({
-      message: 'Internal server error.'
-    });
-  }
 });
 
 module.exports = router;
--- src/user/model.js
+++ src/user/model.js
@@ -2,31 +2,35 @@ const Sequelize = require('sequelize');
 const { database } = require('../database');
 
 const Model = database.define(
-  'user',
-  {
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true
+    'user',
+    {
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: Sequelize.STRING,
+        name: Sequelize.STRING,
+        profilePic: {
+            type: Sequelize.STRING,
+            defaultValue:
+                'https://littlesmilespa.org/wp-content/uploads/2016/08/person-placeholder.png',
+        },
+        rank: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            defaultValue: 0,
+        },
+        newEmail: Sequelize.STRING,
+        validation: {
+            type: Sequelize.STRING,
+            unique: true,
+        },
     },
-    password: Sequelize.STRING,
-    name: Sequelize.STRING,
-    profilePic: Sequelize.STRING,
-    rank: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      defaultValue: 0
-    },
-    newEmail: Sequelize.STRING,
-    validation: {
-      type: Sequelize.STRING,
-      unique: true
+    {
+        tableName: 'users',
+        timestamps: false,
     }
-  },
-  {
-    tableName: 'users',
-    timestamps: false
-  }
 );
 
 module.exports = Model;
